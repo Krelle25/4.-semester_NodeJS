99cd6e1cabc7491e08fb4735ce65dd9bb91bd072
anderslatif <anderslatif@users.noreply.github.com>
2026-02-26 09:10:55 +0100

    Started creating the cookie import / export example

--- 05._HTML_Server/app.js
+++ 05._HTML_Server/app.js
@@ -3,8 +3,16 @@ const app = express();
 
 app.use(express.static('public'));
 
+const cookiesUtil = require('./util/cookiesUtil.js');
+
+// console.log(cookiesUtil.getCookie());
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/frontpage/frontpage.html');
+});
+
+app.get('/cookieFactory', (req, res) => {
+    res.sendFile(__dirname + '/public/cookieFactory/cookieFactory.html');
 });
 
 app.listen(8080, () => {
