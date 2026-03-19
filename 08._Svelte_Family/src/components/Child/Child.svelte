be726a3bf32521b438a15931590df32c917a4c8c
Kristian Meyer Steffensen <krst0003@stud.ek.dk>
2026-03-19 10:03:48 +0100

    Finished the dynamic styling

---
+++ 08._Svelte_Family/src/components/Child/Child.svelte
@@ -0,0 +1,38 @@
+<script>
+    let { name, familySheep, isGirl } = $props();
+</script>
+
+<div
+    class={familySheep || "not-a-sheep"}
+    class:is-girl={isGirl}
+    class:is-boy={!isGirl}
+>
+    <h3>I'm just a child known as { name }.</h3>
+</div>
+
+<style>
+    div {
+        color: white;
+    }
+
+    .black-sheep {
+        background-color: red;
+    }
+
+    .grey-sheep {
+        background-color: lightblue;
+    } 
+
+    .not-a-sheep {
+        background-color: grey;
+    }
+
+    .is-girl {
+        border: 4px dashed pink;
+    }
+
+    .is-boy {
+        border: 4px solid green;
+    }
+
+</style>
---
+++ 08._Svelte_Family/src/components/Home/Home.svelte
@@ -0,0 +1,16 @@
+<slot></slot>
+
+<div id="welcome-mat">
+    <p>Go Away!</p>
+</div>
+
+<style>
+    #welcome-mat {
+        font-weight: bold;
+        color: snow;
+        background-color: rgba(160, 88, 20, 0.599);
+        padding: 2em;
+        margin: 0 20em 0 20em;
+        border-radius: 0 0 2em 2em;
+    }
+</style>
