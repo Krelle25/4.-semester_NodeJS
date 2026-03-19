a02ebeaa1969d31e33fd20098d3eb09a172aa827
anderslatif <anderslatif@users.noreply.github.com>
2026-03-19 09:35:20 +0100

    Created the parents / children structure in Svelte Family

---
+++ 08._Svelte_Family/src/components/Child/Child.svelte
@@ -0,0 +1,10 @@
+<script>
+    let { name, familySheep, isGirl } = $props();
+
+</script>
+
+<div
+    class={familySheep}    
+>
+    <h3>I'm just a child known as {name}</h3>
+</div>
---
+++ 08._Svelte_Family/src/components/Home/Home.svelte
@@ -0,0 +1,17 @@
+
+<slot></slot>
+
+<div id="welcome-mat">
+    <p>Go Away!</p>
+</div>
+
+<style>
+    #welcome-mat {
+        font-weight: bold;
+        color: azure;
+        background-color: brown;
+        padding: 2em;
+        margin: 0 20em 0 20em;
+        border-radius: 0 0 2em 2em;
+    }
+</style>
