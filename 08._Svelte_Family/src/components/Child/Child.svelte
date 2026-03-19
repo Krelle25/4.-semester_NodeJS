86311b2ea6edbccc9bd48a5db0b5979627a171db
anderslatif <anderslatif@users.noreply.github.com>
2026-03-19 10:03:40 +0100

    Finished the dynamic styling for the children

--- 08._Svelte_Family/src/components/Child/Child.svelte
+++ 08._Svelte_Family/src/components/Child/Child.svelte
@@ -1,10 +1,39 @@
 <script>
     let { name, familySheep, isGirl } = $props();
 
+
 </script>
 
 <div
-    class={familySheep}    
+    class={familySheep || "not-a-sheep"}
+    class:is-girl={isGirl}
+    class:is-boy={!isGirl}
 >
     <h3>I'm just a child known as {name}</h3>
 </div>
+
+<style>
+    div {
+        color: white;
+    }
+    
+    .black-sheep {
+        background-color: rebeccapurple;
+    }
+
+    .grey-sheep {
+        background-color: yellowgreen;
+    }
+
+    .not-a-sheep {
+        background-color: plum;
+    }
+
+    .is-girl {
+        border: 4px dashed palegoldenrod;
+    }
+
+    .is-boy {
+        border: 4px solid slateblue;
+    }
+</style>
