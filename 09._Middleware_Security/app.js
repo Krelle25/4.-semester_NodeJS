8391ea33164e815d952d6789ad1af6dcc701894b
anderslatif <anderslatif@users.noreply.github.com>
2026-03-26 10:21:57 +0100

    Set up the rate limiting middleware

--- 09._Middleware_Security/app.js
+++ 09._Middleware_Security/app.js
@@ -3,12 +3,42 @@ import express from 'express';
 const app = express();
 
 
+import { rateLimit } from 'express-rate-limit';
+
+
+const generalLimiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 50, // Limit each IP to 50 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
+	// store: ... , // Redis, Memcached, etc. See below.
+});
+
+app.use(generalLimiter);
+
+
+const authLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000,  // 15 minutes
+    limit: 5,
+    standardHeaders: 'draft-8',
+    legacyHeaders: false,
+    ipv6Subnet: 56,
+});
+
+app.use('/auth', authLimiter);
+
 import helmet from 'helmet';
 app.use(helmet());
 
 
 import middlewareRouter from './routers/middlewareRouter.js';
 app.use(middlewareRouter);
+
+import authRouter from './routers/authRouter.js';
+app.use(authRouter);
+
+
 
 
 // /{*splat} is the new syntax in Express 5.x, before it was just /*
