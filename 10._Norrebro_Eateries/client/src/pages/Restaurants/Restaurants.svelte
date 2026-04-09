b1f1b862a9a15093d1673d49549e646cbcdc179d
Anders Latif <andl@ek.dk>
2026-04-09 09:38:26 +0200

    Created the svelte routing

---
+++ 10._Norrebro_Eateries/client/src/pages/Restaurants/Restaurants.svelte
@@ -0,0 +1,10 @@
+<script>
+
+    fetch('localhost:8080/api/restaurants')
+    .then((response) => response.json())
+    .then((result) => console.log(result));
+
+</script>
+
+
+<h1>Restaurants</h1>
