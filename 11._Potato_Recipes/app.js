4dd9abfb61adad42c7dc929085cc452f9bddd110
Anders Latif <andl@ek.dk>
2026-04-16 10:37:31 +0200

    Finished the database example

--- 11._Potato_Recipes/app.js
+++ 11._Potato_Recipes/app.js
@@ -2,6 +2,10 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
+import recipesRouter from './routers/recipesRouter.js';
+app.use(recipesRouter);
 
 const PORT = process.env.PORT ?? 8080;
 
